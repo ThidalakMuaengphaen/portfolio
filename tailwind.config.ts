ae4e5e52be6caa0b712581ac3df9040f4c28d7df
Thidalak Mueangphaen <thidalak.m@kkumail.com>
2024-11-20 23:22:51 +0700

    white

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "#FFFFFF",
+        background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
